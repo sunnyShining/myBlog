5045b12ed28f6077208ca75a40fe81e3d73d6873
sunny <jdhydca@icloud.com>
2019-01-23 12:22:09 +0800

    deploy aboutMe on master branch

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-rm -rf dist
 function git.branch {
   	br=`git branch | grep "*"`
 }
 git.branch
 git pull origin ${br/* /}:${br/* /}
-yarn build
+yarn d
 git add -A
 git commit -m 'deploy aboutMe on '${br/* /}' branch'
 git push origin ${br/* /}
