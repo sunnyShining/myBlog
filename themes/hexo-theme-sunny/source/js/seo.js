5bdb8584dfa25f6df0d53058cd07d94c03de5ce3
sunny <jdhydca@icloud.com>
2019-01-23 15:45:47 +0800

    deploy blog on master branch

--- themes/hexo-theme-sunny/source/js/seo.js
+++ themes/hexo-theme-sunny/source/js/seo.js
@@ -1,3 +1,7 @@
 var xhr = new XMLHttpRequest();
+const headers = {'Accept': '*/*', 'Content-Type': 'application/x-www-form-urlencoded'};
 xhr.open('POST', 'http://data.zz.baidu.com/urls', true);
+for (var key in headers) {
+    xhr.setRequestHeader(key, headers[key]);
+}
 xhr.send('site=static.sunnyshining.xyz&token=vK3FvKTFdT4axvSs')
